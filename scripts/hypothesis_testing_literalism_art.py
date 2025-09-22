f170b5f9110ecce677206a5a69185fccb045b0b1
Max Vargas <maxvargas@MacBookPro.attlocal.net>
2025-09-22 13:38:40 -0700

    Update testing script

--- scripts/hypothesis_testing_literalism_art.py
+++ scripts/hypothesis_testing_literalism_art.py
@@ -50,11 +50,12 @@ Starting EDA:
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 import cpi
 cpi.update()
 
 # hypothesis 1.1
-file = '/Users/maxvargas/literalism_art_analysis/data/random_sampled_movies_by_year.csv'
+file = '/Users/maxvargas/literalism_art_analysis/data/random_sampled_us_movies_by_year_enriched.csv'
 df = pd.read_csv(file)
 
 ## parse dates, extract year, make budget numeric
@@ -76,7 +77,7 @@ inflation_factors = {
     year: cpi.inflate(1, int(year), to=TARGET_YEAR) for year in unique_years
 }
 
-## Apply cpi: Apply inflation multiplier per row
+## Apply cpi to budget: Apply inflation multiplier per row
 df['adjusted_budget'] = df.apply(
     lambda row: row['budget'] * inflation_factors.get(row['year'], np.nan),
     axis=1
@@ -84,30 +85,83 @@ df['adjusted_budget'] = df.apply(
 df['adjusted_budget'] = df['adjusted_budget'].astype(int)
 df['adjusted_budget'] = df['adjusted_budget'].round(2)
 
+## Apply cpi to revenue: Apply inflation multiplier per row
+df['adjusted_revenue'] = df.apply(
+    lambda row: row['revenue'] * inflation_factors.get(row['year'], np.nan),
+    axis=1
+)
+df['adjusted_revenue'] = df['adjusted_revenue'].astype(int)
+df['adjusted_revenue'] = df['adjusted_revenue'].round(2)
+
+## calculate roi
+df['roi'] = (df['adjusted_revenue'] - df['adjusted_budget']) * 100 / df['adjusted_budget'] 
+
 ## create film_size
 df['film_size'] = np.where(df['adjusted_budget'] <= 15000000, 'Indie',
                   np.where(df['adjusted_budget'] > 100000000, 'Blockbuster', 'Other'))
 
-## create plot dataframe
-grouped = df.groupby(['decade','film_size'])['id'].count().reset_index()
-print(grouped.head(20))
+## create two dataframes
+df_blockbuster = df[df['film_size'] == 'Blockbuster']
+df_indie = df[df['film_size'] == 'Indie']
+df_indie = df[df['year'] > 2014]
 
-## create plot
-grouped_pivot = grouped.pivot(index='decade', columns='film_size', values='id')
-decade_totals = grouped_pivot.sum(axis=1)
-grouped_percent = grouped_pivot.div(decade_totals, axis=0) * 100
-grouped_percent = grouped_percent.round(2)
+## create plot dataframes
+grouped = df.groupby(['decade','film_size','real_true_stories','adapted_inspired_based']).agg({'id': 'count', 'roi': 'mean'}).reset_index()
+grouped_blockbuster = df_blockbuster.groupby(['decade','real_true_stories','adapted_inspired_based']).agg({'id': 'count', 'roi': 'mean'}).reset_index()
+grouped_indie = df_indie.groupby(['year','real_true_stories','adapted_inspired_based']).agg({'id': 'count', 'roi': 'mean'}).reset_index()
 
-ax = grouped_percent.plot(kind='bar', figsize=(10,6), stacked=True, alpha=0.7)
+## create indie real_true_stories decade plot
+grouped_indie_pivot = grouped_indie.pivot_table(index='year', columns='real_true_stories', values='id', aggfunc='sum')
+indie_year_totals = grouped_indie_pivot.sum(axis=1)
+grouped_indie_percent = grouped_indie_pivot.div(indie_year_totals, axis=0) * 100
+grouped_indie_percent = grouped_indie_percent.round(2)
+
+## GRAPH Reorder the columns to ensure desired stack order: 'yes' on bottom, then 'no', then 'maybe'
+desired_order = ['yes', 'no', 'maybe']
+existing_cols = [col for col in desired_order if col in grouped_indie_percent.columns]
+grouped_indie_percent = grouped_indie_percent[existing_cols]
+
+ax = grouped_indie_percent.plot.area(figsize=(10, 6), alpha=0.7)
+
+plt.title("Percent of Indies that are Real/True Stories")
+plt.xlabel("Year")
+plt.ylabel("% of Indies")
+plt.xticks(rotation=45)
+plt.legend(title="Real or True Story")
+plt.tight_layout()
+plt.grid(axis='y', linestyle='--', alpha=0.5)
+plt.legend(loc='lower left')
+plt.show()
+
+## GRAPH create blockbuster adapted_inspired_based decade plot
+grouped_blockbuster_pivot = grouped_blockbuster.pivot_table(index='decade', columns='adapted_inspired_based', values='id', aggfunc='sum')
+blockbuster_decade_totals = grouped_blockbuster_pivot.sum(axis=1)
+grouped_blockbuster_percent = grouped_blockbuster_pivot.div(blockbuster_decade_totals, axis=0) * 100
+grouped_blockbuster_percent = grouped_blockbuster_percent.round(2)
+
+ax = grouped_blockbuster_percent.plot(kind='bar', figsize=(10,6), stacked=True, alpha=0.7)
 
 for container in ax.containers:
     ax.bar_label(container, label_type='center', fontsize=8, color='white')
 
-plt.title("Percent of Movies by Film Size and Decade")
+plt.title("Percent of Blockbusters that are Adapted/Inspired")
 plt.xlabel("Decade")
-plt.ylabel("% of Movies")
+plt.ylabel("% of Blockbusters")
 plt.xticks(rotation=45)
-plt.legend(title="Budget Type")
+plt.legend(title="Adapted or Inspired Movie")
 plt.tight_layout()
 plt.grid(axis='y', linestyle='--', alpha=0.5)
+plt.legend(loc='lower left')
 plt.show()
+
+## GRAPH create roi heatmap
+heatmap_data = grouped_blockbuster.pivot_table(index='decade', columns=['adapted_inspired_based'], values='roi',aggfunc='mean')
+
+plt.figure(figsize=(10, 6))
+sns.heatmap(heatmap_data, annot=True, fmt=".1f", cmap="coolwarm", linewidths=0.5, linecolor='white')
+plt.title("Average ROI by Decade and Adapted/Inspired Movie")
+plt.ylabel("Decade")
+plt.tight_layout()
+plt.show()
+
+
